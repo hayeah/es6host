2e88b8ff9e2a055c030e7ecd036f99b6b9a3dcfb
Howard Yeh <howard@metacircus.com>
2017-07-24 18:19:29 +0800

    init

---
+++ README.md
@@ -0,0 +1,28 @@
+Chrome [Canary supports ES6 modules](https://medium.com/dev-channel/es6-modules-in-chrome-canary-m60-ba588dfb8ab7). It's pretty great. No more Rube Goldberg setup is necessary to develop modern JavaScript app.
+
+This is a simple Go web server to support common ES6 app structure.
+
+# Instruction
+
+Include the `index.js` of your app in `index.html`:
+
+```
+<script type="module" src="index.js"></script>
+```
+
+Inside any source file, you may import a module using relative path:
+
+```
+import foo from "./foo";
+console.log("hello ES6 module");
+```
+
+Run the web server:
+
+```
+es6host -root ./
+```
+
+# TODO
+
+* Support for node_modules lookup.
---
+++ main.go
@@ -0,0 +1,114 @@
+// Web server that supports ES6 modules.
+//
+// By convention ES6 programmers like to omit module extension names.
+// All this module does is to attempt to append ".js" when looking
+// up the file for a module.
+package main
+
+import (
+	"flag"
+	"io"
+	"log"
+	"mime"
+	"net/http"
+	"os"
+	"path"
+)
+
+var rootDir string
+var serveAddr string
+
+func init() {
+	flag.StringVar(&rootDir, "root", "./", "root directory for static resources")
+	flag.StringVar(&serveAddr, "bind", "127.0.0.1:8888", "server address to bind to")
+}
+
+func main() {
+	flag.Parse()
+
+	err := serve()
+	if err != nil {
+		log.Fatalln("start server:", err)
+	}
+}
+
+func serve() error {
+	s := &es6Server{
+		root: "./",
+	}
+
+	return http.ListenAndServe(":7777", s)
+}
+
+type es6Server struct {
+	root string
+}
+
+func (s *es6Server) ServeHTTP(res http.ResponseWriter, req *http.Request) {
+	var err error
+	defer func() {
+		if err != nil {
+			log.Println("request err", err)
+			res.WriteHeader(500)
+			res.Write([]byte(err.Error()))
+		}
+	}()
+
+	filePath := path.Join(s.root, req.RequestURI)
+
+	log.Println("GET", filePath, req.Header.Get("Content-Type"))
+
+	exts := []string{".js"}
+
+	i := 0
+
+	var stat os.FileInfo
+	lookupFilePath := filePath
+	for {
+		stat, err = os.Stat(lookupFilePath)
+		if err != nil && !os.IsNotExist(err) {
+			return
+		}
+
+		if os.IsNotExist(err) {
+			if i >= len(exts) {
+				return
+			}
+
+			lookupFilePath = filePath + exts[i]
+			i++
+			continue
+		}
+
+		break
+	}
+
+	// Serve directory index
+	if stat.IsDir() {
+		if req.RequestURI[len(req.RequestURI)-1] != '/' {
+			res.Header().Set("Location", "/"+filePath+"/")
+			res.WriteHeader(302)
+			return
+		}
+
+		lookupFilePath = path.Join(filePath, "index.html")
+	}
+
+	if filePath != lookupFilePath {
+		log.Println("\t", lookupFilePath)
+	}
+
+	f, ferr := os.Open(lookupFilePath)
+	if ferr != nil {
+		err = ferr
+		return
+	}
+	defer f.Close()
+
+	chosenExt := path.Ext(lookupFilePath)
+	if chosenExt != "" {
+		res.Header().Set("Content-Type", mime.TypeByExtension(chosenExt))
+	}
+
+	io.Copy(res, f)
+}
